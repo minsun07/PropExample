85d7b7f9f65927288cc1c319a3024d6f92c338fd
Minsun An <elleto123456@gmail.com>
2025-05-28 10:10:38 +0900

    오버로딩 주의점

--- Program.cs
+++ Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace PropExample
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            TestOver(3);
         }
+
+        // 오버로딩 주의점
+        public static int TestOver(int input) { return 0; }
+        public static bool TestOver(double input2) { return true; }
     }
 }
