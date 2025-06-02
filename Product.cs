b611f35932a36f2d37b0204aab3147ad5576eba4
Minsun An <elleto123456@gmail.com>
2025-06-02 13:43:56 +0900

    싱글톤 패턴

--- Product.cs
+++ Product.cs
@@ -12,12 +12,17 @@ namespace PropExample
     {
         public string name;
         public int price;
+        private static Product product;
 
 
         // 팩토리 메서드 패턴
         public static Product getInstance(string name, int price)
         {
-            return new Product(name, price);
+            if(product == null)
+            {
+                product = new Product(name, price);
+            }
+            return product;
         }
 
         public Product(string name, int price)
