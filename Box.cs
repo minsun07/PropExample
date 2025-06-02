646b1f3684160214f65f09673b23bf6dc057cca2
Minsun An <elleto123456@gmail.com>
2025-06-02 14:57:36 +0900

    getter setter 방식으로 구현

--- Box.cs
+++ Box.cs
@@ -24,6 +24,37 @@ namespace PropExample
             }
           
         }
+
+        public int getWidth()
+        {
+            return width;
+        }
+        public int getHeight()
+        {
+            return height;
+        }
+
+        public void setWidht(int width)
+        {
+            if(width > 0) {
+                this.width = width;
+            }
+            else
+            {
+                Console.WriteLine("너비는 저연수로 초기화해수제쇼.")
+            }
+        }
+        public void setHeight(int height)
+        {
+            if(height > 0)
+        {
+            this.height = height;
+        }else
+          {
+              Console.WriteLine("높이는 자연수로 초기화 해주세요.");
+        }
+            
+        }
         public int Area() { return width * height; }
     }
 }
